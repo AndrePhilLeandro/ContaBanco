ae42a4f95a163d114a059e18619ea60acf4fafc8
AndreLeandro <andrephilleandro@gmail.com>
2023-09-07 12:05:13 -0300

    Programa de teste DIO Conta Bancaria

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+    Scanner digitar = new Scanner(System.in).useLocale(Locale.US); 
+    Random random = new Random();
+        
+    int conta = random.nextInt(1000, 2000);
+
+
+    System.out.println("Por Favor, digite o numero da Agencia !");
+     int Ag = digitar.nextInt();
+     digitar.nextLine();
+     
+    
+    System.out.println("Por Favor, Digite seu nome completo! ");
+      String nome = digitar.next();
+      digitar.nextLine();
+      
+
+    System.out.println("Por Favor, Digite o valor do seu primeiro deposito !");
+      double saldo =  digitar.nextDouble();
+    
+       System.out.println("Seu Saldo Atual é :" + saldo );
+       
+    int numero;
+    System.out.println("Óla " + nome + ", obrigado por criar conta em nosso banco,\nSua agencia é " + Ag + "\nE sua conta é " +conta+ "\nSeu saldo Atual é " + saldo + " é esta disponivel para saque! ");
+}
 }
